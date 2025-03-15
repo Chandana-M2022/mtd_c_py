57da4080119a9557d3c4cc421cabd92d76b61a68
Chandana-M2022 <chandana.m2916@gmail.com>
2025-03-15 15:33:33 +0530

    mtd

--- day6/p2.py
+++ day6/p2.py
@@ -1,6 +1,7 @@
 from prime import check_is_prime
-x= int(input('Enter the value for x: '))
-y=int(input('Enter the value for y:'))
+x= int(input('Enter the start value: '))
+y=int(input('Enter the end value:'))
+print('The prime numbers between start value and end value are')
 for number in range(x,y+1):
     if check_is_prime(number):
-        print(number)
+        print(number,end=" ")
---
+++ day6/snehaprob.py
@@ -0,0 +1,7 @@
+import math
+n=int(input("Enter the number of pair of shoes available"))
+p=int(input("Enter the number of pair the customer can carry"))
+for price in range(1,n+1):
+    nPrice = int(input("Enter the price of the shoes"))
+for profit in range(1,p+1):
+        
